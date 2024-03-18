f70faba295033cde08a9250bb8b5b188e34d5f03
nocturnum91 <nocturnum91@gmail.com>
2024-03-19 00:52:02 +0900

    Spring Boot 버전 2.1.18 -> 2.7.18로 업그레이드, 설정파일 수정, DBTests 클래스에 TestPropertySource 추가

--- README.md
+++ README.md
@@ -16,11 +16,11 @@
 Clone this repository and navigate to the project directory. Run the following command to execute a specific job:
 
 ```bash
-java -jar jarname --job.name=jobname
+java -jar jarname --spring.batch.job.names=jobname
 ```
 
 For example, to execute the `selectInsertBatchJob` or `selectDeleteBatchJob` job, run the following command:
 
 ```bash
-java -jar SpringBatch-0.0.1-SNAPSHOT.jar --job.name=selectInsertBatchJob;
-java -jar SpringBatch-0.0.1-SNAPSHOT.jar --job.name=selectDeleteBatchJob;
+java -jar SpringBatch-0.0.1-SNAPSHOT.jar --spring.batch.job.names=selectInsertBatchJob;
+java -jar SpringBatch-0.0.1-SNAPSHOT.jar --spring.batch.job.names=selectDeleteBatchJob;
--- src/test/java/org/nocturnum/batch/DBTests.java
+++ src/test/java/org/nocturnum/batch/DBTests.java
@@ -10,14 +10,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
 
 @Slf4j
 @SpringBootTest(properties = "spring.config.location=classpath:/application.yml, classpath:/config/datasource.yml")
-@ActiveProfiles("local")
+@TestPropertySource(properties = "spring.batch.job.enabled=false")
 class DBTests {
 
     @Value("${server.port}")
